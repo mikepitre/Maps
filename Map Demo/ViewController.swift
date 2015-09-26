a9342d1d828bff9000722afeee5a3fed404bb118
mikepitre <michaelepitre@gmail.com>
2015-09-26 11:44:12 -0500

    user can add new annotations using a long press

--- Map Demo/ViewController.swift
+++ Map Demo/ViewController.swift
@@ -16,15 +16,43 @@ class ViewController: UIViewController, MKMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var latitude:CLLocationDegrees = 40.7
-        var longitude:CLLocationDegrees = -73.9
-        var latDelta:CLLocationDegrees = 0.01
-        var lonDelta:CLLocationDegrees = 0.01
-        var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
-        var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
-        var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        //Niargra Falls: 43.082816, -79.074200
+        
+        let latitude:CLLocationDegrees = 43.082816
+        let longitude:CLLocationDegrees = -79.074200
+        let latDelta:CLLocationDegrees = 0.05
+        let lonDelta:CLLocationDegrees = 0.05
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
         
         map.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Niagra Falls"
+        annotation.subtitle = "Iconic, monumental group of waterfalls"
+        
+        map.addAnnotation(annotation)
+        
+        let uilpgr = UILongPressGestureRecognizer(target: self, action: "action:")
+        uilpgr.minimumPressDuration = 2
+        
+        map.addGestureRecognizer(uilpgr)
+        
+    }
+    
+    func action(gestureRecognizer:UIGestureRecognizer) {
+        
+        let touchPoint = gestureRecognizer.locationInView(self.map)
+        let newCoordinate: CLLocationCoordinate2D = map.convertPoint(touchPoint, toCoordinateFromView: self.map)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = newCoordinate
+        annotation.title = "New Place"
+        annotation.subtitle = "I'd like to go here"
+        
+        map.addAnnotation(annotation)
         
     }
 
